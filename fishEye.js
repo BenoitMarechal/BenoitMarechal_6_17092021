c4aff3fb5860efd21315cbf204e5c83ffb0fc1e6
Benoit Maréchal <benoit.marechal.56@gmail.com>
2021-10-06 17:24:23 +0200

    fonction NewArtist OK

--- fishEye.js
+++ fishEye.js
@@ -1,17 +1,15 @@
 let dataFromJson = [];
 
 async function global() {
+	removeAllArticles();
 	await extractData(); //gets data from json
-	//await buildTagNavBar();
-	//await buildAllArtistsArticles(); //builds tag Nav bar
-	//await clickOnNavTag(); //manages color
-	//await newArtist2(elementsOfArticle);
-	//builds ALL artists articles
-	//	await findArtistFromClickOnTag('travel');
-
-	//await buildArtistsArticleByName('Mimi Keel');
+	await buildTagNavBar();
+	await buildAllArtistsArticles(); //builds tag Nav bar
+	await clickOnNavTag(); //manages color
 }
 global();
+
+//////////////////////FETCH/////////////////
 
 async function extractData() {
 	let rep = await fetch('./public/dataBase.json');
@@ -19,35 +17,14 @@ async function extractData() {
 	return dataFromJson;
 }
 
-async function buildArtistsArticleByName(name) {
-	dataFromJson.photographers.forEach((photographer) => {
-		if (photographer.name === name) {
-			newArtist(
-				photographer.name,
-				photographer.city,
-				photographer.country,
-				photographer.tagline,
-				photographer.price,
-				photographer.tags
-			);
-		}
-	});
+//////////////////////END OF FETCH/////////////////
+
+////////////////////UTILITARIES///////////////////////////////////////////////////
+function removeAllArticles() {
+	//used both in filter management and page initialisation, remove all photographer articles and sets up H1
+	main.innerHTML = '<h1>Nos photographes</h1>';
 }
 
-async function buildAllArtistsArticles() {
-	dataFromJson.photographers.forEach((photographer) => {
-		newArtist(
-			photographer.name,
-			photographer.city,
-			photographer.country,
-			photographer.tagline,
-			photographer.price,
-			photographer.tags
-		);
-	});
-}
-
-//utilitaries
 function changeBoolean(boolean) {
 	if (boolean === true) {
 		boolean = false;
@@ -65,6 +42,19 @@ function removeSpacesInString(string) {
 function removeHashTagsInString(string) {
 	// var reg = /[ ,-]/g;
 	return string.replace('#', '');
+}
+function removeDotsInString(string) {
+	// var reg = /[ ,-]/g;
+	return string.replace('.', '');
+}
+
+function removeElementFromArray(array, element) {
+	for (i = 0; i < array.length; i++) {
+		if (array[i] === element) {
+			array = array.splice(i);
+		}
+	}
+	return array;
 }
 
 function generateTagButtons(tagsArray) {
@@ -92,107 +82,86 @@ let elementsOfArticle = {
 		'photographer__link__tags',
 	],
 	parentOfElement: [
-		'.main',
-		'.photographer',
-		'.photographer__link',
-		'.photographer__link',
-		'.photographer__link',
-		'.photographer__link__location',
-		'.photographer__link__location',
-		'.photographer__link',
-		'.photographer__link',
-		'.photographer__link',
+		'main',
+		'photographer',
+		'photographer__link',
+		'photographer__link',
+		'photographer__link',
+		'photographer__link__location',
+		'photographer__link__location',
+		'photographer__link',
+		'photographer__link',
+		'photographer__link',
 	],
 };
-let arrayOfElements = [];
 
-function newArtist2(object, name) {
+////////////////////END OF UTILITARIES///////////////////////////////////////////////////
+
+//////////////////////////////CREATING ARTISTS ARTICLES///////////////////
+
+function newArtist(object, name, city, country, tagline, price, tags) {
+	let clearedName = removeSpacesInString(name);
 	for (let i = 0; i < object.typeOfElement.length; i++) {
 		let element = document.createElement(object.typeOfElement[i]);
 		element.classList.add(object.classOfElement[i]);
-		name = removeSpacesInString(name);
-		let tour = i;
+		let tour = i; //assigns photg name as article ID
 		if (tour === 0) {
-			console.log('yep');
-			element.id = name;
+			element.id = clearedName;
 		}
-
-		console.log(i);
-
-		console.log(element);
-		let parent = document.querySelector(object.parentOfElement[i]);
-		//console.log(parent);
+		let byClass = document.getElementsByClassName(object.parentOfElement[i]);
+		let parent = byClass.item(byClass.length - 1); //declares last element of collection as parent
 		parent.appendChild(element);
-		// if ((i = 0)) {
-		// 	console.log('yay');
-		// }
-		//elementsOfArticle.parentOfElement[i].appendChild(element);
-
-		// arrayOfElements.push(element);
-		// //main.appendChild(element);
-
-		// console.log(arrayOfElements);
-	}
-	console.log('loop finished');
+	} //EMPTY ARTICLE CREATED
+	articleToFill = document.getElementById(clearedName);
+	articleToFill.querySelector('.photographer__link').href = '';
+	articleToFill.querySelector('.photographer__link__img').src =
+		'images/Photographers ID Photos/' + removeSpacesInString(name) + '.jpg';
+	articleToFill.querySelector('.photographer__link__img').alt = name;
+	articleToFill.querySelector('.photographer__link__name').innerText = name;
+	articleToFill.querySelector('.photographer__link__location__city').innerText =
+		city + ', ';
+	articleToFill.querySelector(
+		'.photographer__link__location__country'
+	).innerText = country;
+	articleToFill.querySelector('.photographer__link__tagline').innerText =
+		tagline;
+	articleToFill.querySelector('.photographer__link__price').innerText =
+		price + ' €/jour';
+	articleToFill.querySelector('.photographer__link__tags').innerHTML =
+		generateTagButtons(tags); //ARTICLE COMPLETED
 }
 
-newArtist2(elementsOfArticle, 'Mimi Keel');
-//newArtist2(elementsOfArticle);
+async function buildArtistArticleByName(name) {
+	dataFromJson.photographers.forEach((photographer) => {
+		if (photographer.name === name) {
+			newArtist(
+				elementsOfArticle,
+				photographer.name,
+				photographer.city,
+				photographer.country,
+				photographer.tagline,
+				photographer.price,
+				photographer.tags
+			);
+		}
+	});
+}
 
-// <!-- <article class="photographer">
-// 				<a href="" class="photographer__link">
-// 					<img
-// 						class="photographer__link__img"
-// 						src="images\Photographers ID Photos\MimiKeel.jpg"
-// 						alt=""
-// 					/>
-// 					<h2 class="photographer__link__name">Mimi Keel</h2>
-// 					<div class="photographer__link__location">
-// 						<p class="photographer__link__location__city">London</p>
-// 						<p class="photographer__link__location__country">, UK</p>
-// 					</div>
-
-// 					<p class="photographer__link__tagline">
-// 						Voir le beau dans le quotidien
-// 					</p>
-// 					<p class="photographer__link__price">400€/jour</p>
-// 					<ul class="photographer__link__tags">
-// 						<li><button class="tag">#Portait</button></li>
-// 						<li><button class="tag">#Events</button></li>
-// 						<li><button class="tag">#Travel</button></li>
-// 						<li><button class="tag">#Animals</button></li>
-// 					</ul>
-// 				</a>
-// 			</article> -->
-
-function newArtist(name, city, country, tagline, price, tags) {
-	//faire plusieurs ajout d'elements plut^to qu'une grosse ligne
-
-	let photographerPresentation = document.createElement('article'); //creates an article
-	photographerPresentation.classList.add('photographer'); // Adds a class
-	photographerPresentation.innerHTML = //fills the article
-		'<a href="" class="photographer__link"><img class="photographer__link__img" src="images/Photographers ID Photos/' +
-		removeSpacesInString(name) + //inserts profile picture
-		'.jpg" alt=""/><h2 class="photographer__link__name">' +
-		name + //inserts name
-		'</h2><div class="photographer__link__location"><p class="photographer__link__location__city">' +
-		city + //inserts city
-		'</p><p class="photographer__link__location__country">, ' +
-		country + //inserts COUNTRY
-		'</p></div><p class="photographer__link__tagline">' +
-		tagline + //inserts TAGLINE
-		'</p><p class="photographer__link__price">' +
-		price + //inserts PRICE
-		'€/jour</p><ul class="photographer__link__tags">' +
-		generateTagButtons(tags) +
-		'</ul></a>';
-
-	main.appendChild(photographerPresentation); //add the article to the bottom of the list
+async function buildAllArtistsArticles() {
+	dataFromJson.photographers.forEach((photographer) => {
+		newArtist(
+			elementsOfArticle,
+			photographer.name,
+			photographer.city,
+			photographer.country,
+			photographer.tagline,
+			photographer.price,
+			photographer.tags
+		);
+	});
 }
 
 //////////////////////////////END OF CREATING ARTISTS ARTICLES///////////////////
-
-//////////////////////////////MANAGING FILTERS ///////////////////
 
 ///////////////////////BUILDING TAG BUTTONS IN NAV BAR/////////////////////////
 //declare constants
@@ -217,33 +186,9 @@ async function buildTagNavBar() {
 
 /////////END OF BUILDING NAV BAR nav bar with all tags
 
-/////////MANAGE FILTER SELECTION//////////////////
+//////////////////////////////MANAGING FILTERS ///////////////////
 
-// currentTagSelection.tag = everyTagArray;
-
-//function initTags() {
-// for (var i = 0; i < currentTagSelection.tag.length; i++) {
-// 	currentTagSelection.selected.push(false);
-// }
-
-// element = document.getElementsByTagName('label');
-// for (index = 0; index < element.length; index++) {
-// 	element[index].parentNode.removeChild(element[index]);
-// }
-// function removeAllArticles() {
-// 	allArticles = document.getElementsByClassName('photographer');
-// 	console.log(allArticles);
-// 	allArticles.forEach((article) => {
-// 		main.removeChild(article);
-// 	});
-// 	console.log('fini ');
-// }
-
-function removeAllArticles() {
-	main.innerHTML = '<h1>Nos photographes</h1>';
-}
-
-let selectedTags = [];
+let activeTags = [];
 
 async function clickOnNavTag() {
 	allNavBtn = document.querySelectorAll('#header__nav__ul button');
@@ -251,82 +196,38 @@ async function clickOnNavTag() {
 
 	allNavBtn.forEach((btn) => {
 		let selected = false;
-		//let selectedTags = [];
+
 		btn.addEventListener('click', function (e) {
 			//looping through html buttons
 			removeAllArticles();
-			//let selectedTags = [];
 			selected = changeBoolean(selected); //change state of individual buttons
 			let currentTag = removeHashTagsInString(btn.innerText);
 
 			if (selected === true) {
 				btn.className = 'tag--On';
-				selectedTags.push(currentTag);
-				//console.log(selectedTags);
-				//findArtistFromClickOnTag(currentTag);
+				activeTags.push(currentTag);
 			} else {
 				btn.className = 'tag--Off';
-				removeElementFromArray(selectedTags, currentTag);
+				removeElementFromArray(activeTags, currentTag);
 			}
 
-			if (selectedTags.length === 0) {
+			if (activeTags.length === 0) {
 				buildAllArtistsArticles();
 			}
 
-			findArtistFromClickOnTag(selectedTags);
+			findArtistFromClickOnTag(activeTags);
 
-			return selectedTags;
+			return activeTags;
 		});
 	});
 }
-
-// function clickOnNavTag() {
-// 	allNavBtn = document.querySelectorAll('#header__nav__ul button');
-// 	//allArtistArticles = document.getElementsByClassName('photographer');
-// 	for (i = 0; i < allNavBtn.length; i++) {
-// 		selectedTags.push(allNavBtn[i].innerText);
-// 		allNavBtn[i].addEventListener('click', function (e) {
-
-// 		});
-// 	}
-// }
-
-function removeElementFromArray(array, element) {
-	for (i = 0; i < array.length; i++) {
-		if (array[i] === element) {
-			array = array.splice(i);
-		}
-	}
-	return array;
-}
-
-// function findArtistFromClickOnTag(array) {
-// 	console.log(dataFromJson);
-// 	console.log(selectedTags);
-// 	console.log(dataFromJson.photographers);
-// 	for (var a = 0; a < array.length; a++) {
-// 		//looping through selected tags
-// 		//dataFromJson.photographers.forEach((photographer) => {
-// 		for (var b = 0; b < dataFromJson.photographers.length; b++) {
-// 			//loop trough photograpahers
-// 			for (var i = 0; i < dataFromJson.photographers[b].tags.length; i++) {
-// 				//loop though photgrahper's tags
-// 				if (dataFromJson.photographers[b].tags[i] === array[a]) {
-// 					console.log(dataFromJson.photographers[b].name);
-// 					buildArtistsArticleByName(dataFromJson.photographers[b].name);
-// 					b = dataFromJson.photographers.length;
-// 				}
-// 			}
-// 		}
-// 	}
-// }
 
 function findArtistFromClickOnTag(array) {
 	for (var a = 0; a < dataFromJson.photographers.length; a++) {
 		for (var b = 0; b < dataFromJson.photographers[a].tags.length; b++) {
 			for (var c = 0; c < array.length; c++) {
 				if (dataFromJson.photographers[a].tags[b] === array[c]) {
-					buildArtistsArticleByName(dataFromJson.photographers[a].name);
+					buildArtistArticleByName(dataFromJson.photographers[a].name);
 					b = dataFromJson.photographers[a].tags.length;
 				}
 			}
