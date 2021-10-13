505437465e8e0c3cdec913e7fa1d35f07086036b
Benoit Maréchal <benoit.marechal.56@gmail.com>
2021-10-13 17:52:18 +0200

    correction methode create

--- javaScript/global.js
+++ javaScript/global.js
@@ -3,7 +3,7 @@ async function global() {
 	await extractData(); //gets data from json
 	await fillarrayOfArtists(); //generates array of artists
 	await buildTagNavBar(); //builds nav bar tags
-	await buildAllArtistsArticles2(); //builds all artists articles
+	await buildAllArtistsArticles(); //builds all artists articles
 	await clickOnNavTag(); //manages color
 	//await buildArtistArticleByName2(); ////builds selected artists articles
 }
